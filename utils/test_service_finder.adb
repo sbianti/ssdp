c7d1968a9532d3fd5dfcc038cb70878cb7f0ef38
Sébastien Bianti <sebastienbianti@free.fr>
2015-12-01 01:01:09 +0100

    Rewrite unit-tests prior to use a more complex usage
    Prior to use complex strategies with --batch option, group default
    behaviour in one procedure.
    
    Signed-off-by: Sébastien Bianti <sebastienbianti@free.fr>

--- utils/test_service_finder.adb
+++ utils/test_service_finder.adb
@@ -1,30 +1,57 @@
-with Ada.Text_Io;
+--------------------------------------------------------------------------------
+--  Test_Service_Finder 						      --
+--  									      --
+--  Copyright © 2015 Sébastien Bianti					      --
+--  									      --
+--  This program is free software; you can redistribute it and/or modify      --
+--  it under the terms of the GNU General Public License version 2 as	      --
+--  published by the Free Software Foundation.				      --
+--  									      --
+--  This program is distributed in the hope that it will be useful,	      --
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of	      --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	      --
+--  GNU General Public License for more details.			      --
+--  									      --
+--  You should have received a copy of the GNU General Public License	      --
+--  along with this program; if not, write to the Free Software	              --
+--  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA --
+--------------------------------------------------------------------------------
+
+with Ada.Text_IO;
 with SSDP.Service_Finder;
 
+with Get_Options; --https://github.com/sbianti/GetOptions
+
 procedure Test_Service_Finder is
-   use Ssdp.Service_Finder, Ada.Text_Io;
+   use SSDP.Service_Finder, Ada.Text_IO;
 
-   Discover_Header: SSDP.Message_Header_Array := (To_US("Toto: inutile:pardon"),
-						  To_US("Zak: important:rien"));
+   procedure Default_Scheduling is
+      Discover_Header: SSDP.Message_Header_Array :=
+	(To_US("Toto: inutile:pardon"), To_US("Zak: important:rien"));
 
-   Null_Header: SSDP.Message_Header_Array(1..0);
+      Null_Header: SSDP.Message_Header_Array(1..0);
 
-   UUID: constant String := "uuid:0dbcf247-96ca-4d58-b3de-a22cd083125b";
+      UUID: constant String := "uuid:0dbcf247-96ca-4d58-b3de-a22cd083125b";
 
-   Device: Finder_Device_Type := Initialize_Device("sncf:TGV", UUID);
-   Str: String(1..10);
-   Lg: Natural;
+      Device: Finder_Device_Type := Initialize_Device("sncf:TGV", UUID);
+      Str: String(1..10);
+      Lg: Natural;
+   begin
+      Start_Listening;
+
+      delay 0.5;
+      M_Search(Device, Discover_Header);
+      Put_Line("First discover sent");
+
+      delay 1.0;
+      M_Search(Device, NULL_Header);
+      Put_Line("Second discover sent");
+
+      Get_Line(Str, Lg);
+
+      Stop_Listening;
+   end Default_Scheduling;
+
 begin
-   Start_Listening;
-   delay 0.5;
-
-   Put_Line("Envoie du Discover:");
-   M_Search(Device, Discover_Header);
-   delay 1.0;
-
-   Put_Line("Envoie du deuxième Discover:");
-   M_Search(Device, NULL_Header);
-
-   Get_Line(Str, Lg);
-   Stop_Listening;
+   Default_Scheduling;
 end Test_Service_Finder;
--- utils/test_service_provider.adb
+++ utils/test_service_provider.adb
@@ -1,36 +1,64 @@
+--------------------------------------------------------------------------------
+--  Test_Service_Provider						      --
+--  									      --
+--  Copyright © 2015 Sébastien Bianti					      --
+--  									      --
+--  This program is free software; you can redistribute it and/or modify      --
+--  it under the terms of the GNU General Public License version 2 as	      --
+--  published by the Free Software Foundation.				      --
+--  									      --
+--  This program is distributed in the hope that it will be useful,	      --
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of	      --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	      --
+--  GNU General Public License for more details.			      --
+--  									      --
+--  You should have received a copy of the GNU General Public License	      --
+--  along with this program; if not, write to the Free Software	              --
+--  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA --
+--------------------------------------------------------------------------------
+
 with Ada.Text_IO;
 with SSDP.Service_Provider;
 
 procedure Test_Service_Provider is
    use Ada.Text_IO, SSDP.Service_Provider;
 
-   Notify_Header: SSDP.Message_Header_Array := (To_US("Affaires: dentifrice"),
-						To_US("DIY: courses:rien"));
+    procedure Default_Scheduling is
+      Notify_Header: SSDP.Message_Header_Array :=
+	(To_US("Affaires: dentifrice"), To_US("DIY: courses:rien"));
 
-   UUID: constant String := "822ccbbf-3aa6-44c2-80ef-0307f9673521";
+      UUID: constant String := "822ccbbf-3aa6-44c2-80ef-0307f9673521";
 
-   Null_Header: SSDP.Message_Header_Array(1..0);
+      Null_Header: SSDP.Message_Header_Array(1..0);
 
-   Device: Service_Provider_Device_Type :=
-     Initialize_Device(Service_Type => "chauffage:central",
-		       Universal_Serial_Number => UUID,
-		       Location => "",
-		       AL => "<http://halsensortester.mp.intel.com>",
-		       Cache_Control => "max-age = 600",
-		       Expires => "");
+      Device: Service_Provider_Device_Type :=
+	Initialize_Device(Service_Type => "chauffage:central",
+			  Universal_Serial_Number => UUID,
+			  Location => "",
+			  AL => "<http://halsensortester.mp.intel.com>",
+			  Cache_Control => "max-age = 600",
+			  Expires => "");
 
-   Str: String(1..10);
-   Lg: Natural;
+      Str: String(1..10);
+      Lg: Natural;
+   begin
+      Start_Listening;
+
+      delay 0.2;
+      Notify_Alive(Device, Notify_Header);
+      Put_Line("Notify sent");
+
+      delay 2.5;
+      Notify_Alive(Device, Null_Header);
+      Put_Line("Second notify sent");
+
+      Get_Line(Str, Lg);
+      Put_Line("Bye bye !");
+      Notify_Bye_Bye(Device);
+
+      Stop_Listening;
+   end Default_Scheduling;
+
 begin
-   Start_Listening;
-   delay 0.2;
-
-   Put_Line("Envoie du Notify:");
-   Notify_Alive(Device, Notify_Header);
-
-   Get_Line(Str, Lg);
-   Put_Line("Au revoir !");
-   Notify_Bye_Bye(Device);
-
-   Stop_Listening;
+   Default_Scheduling;
 end Test_Service_Provider;
